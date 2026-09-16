87d3158766c6ff35f7055ecfaec914b38a691dd0
TuhinBanerjee31 <tuhinbanerjee0231@gmail.com>
2026-09-17 00:43:23 +0530

    done

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
-# cd ci-cd-test-project
-# git pull origin main
-# npm install
-# npm run build
-# npm run start
-
-ls
+cd ci-cd-test-project
+git pull origin main
+npm install
+npm run build
+npm run start
